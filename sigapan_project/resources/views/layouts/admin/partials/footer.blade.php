f4f5bcad3a6ccb68b2d75959f68b40ee96fe6fe9
Septian Jati <septianjati820@gmail.com>
2026-01-21 14:50:22 +0700

    WIP: local changes

--- sigapan_project/resources/views/layouts/admin/partials/footer.blade.php
+++ sigapan_project/resources/views/layouts/admin/partials/footer.blade.php
@@ -1,5 +1,7 @@
 <footer class="bg-white dark:bg-[#0c1427] rounded-t-md px-[20px] md:px-[25px] py-[15px] md:py-[20px] text-center">
     <p>
-        <span class="text-purple-500">{{ $prefs_composer['title'] }}</span> {!! $prefs_composer['copyright'] !!} {!! $prefs_composer['credits'] !!}
-    </p>
+    <span class="text-purple-500">{{ $prefs_composer['title'] ?? 'Judul Default' }}</span> 
+    {!! $prefs_composer['copyright'] ?? '© 2026' !!} 
+    {!! $prefs_composer['credits'] ?? '' !!}
+</p>
 </footer>
